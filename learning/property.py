2b7dd302741c2fba3b4dd44fd8e73d064e483a5d
taccisum <514162920@qq.com>
2018-08-20 15:06:06 +0800

    use assert print

--- learning/property.py
+++ learning/property.py
@@ -22,34 +22,29 @@ class Foo3(Foo1):
     pass
 
 
-print(Foo().bar)
+assert Foo().bar is '0'
 foo1 = Foo1()
-print(foo1.bar)
+assert foo1.bar is '1'
 foo1.bar1 = '_1'
-print(foo1.bar1)
-print(Foo2()._bar)
+assert foo1.bar1 is '_1'
+assert Foo2()._bar is '2'
 # print(Foo2().__bar)   # can not access '__bar' via this way
-print(Foo2()._Foo2__bar)
-print(Foo3().bar)
-
-
-print()
-print('#'*10)
-print()
+assert Foo2()._Foo2__bar is '_2'
+assert Foo3().bar is '1'
 
 
 class Clsvar(object):
     foo = '123'
 
 
-print(Clsvar().foo)
+assert Clsvar().foo is '123'
 clsvar1 = Clsvar()
 clsvar2 = Clsvar()
-print(clsvar1.foo)
-print(clsvar2.foo)
+assert clsvar1.foo is '123'
+assert clsvar2.foo is '123'
 clsvar1.foo = '321'
-print(clsvar1.foo)
-print(clsvar2.foo)
+assert clsvar1.foo is '321'
+assert clsvar2.foo is '123'
 Clsvar.foo = 'abc'
-print(clsvar1.foo)
-print(clsvar2.foo)
+assert clsvar1.foo is '321'
+assert clsvar2.foo is 'abc'
